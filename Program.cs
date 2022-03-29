b5796d39b77d2e971f99279a4856af86ebbf6f8b
maustin1 <mtv1243@gmail.com>
2022-03-29 10:38:35 -0400

    first commit. coding tracker will log your time coding. part of thecsharpacademy.com

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coding_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
